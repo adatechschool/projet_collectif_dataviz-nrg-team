5384c09209e64b840430bb878136cd4dd216d426
huor97 <huor97ka@gmail.com>
2022-11-22 17:00:15 +0100

    avoir année par la recherch d'un utlisateur

--- api.js
+++ api.js
@@ -1,40 +1,39 @@
+// ==================== Avoir api
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '6c3825cd5dmsh566a0d01b2db89fp1c843cjsn018abed16448',
 		'X-RapidAPI-Host': 'netflix-api3.p.rapidapi.com'
 	}
-}; 
+};
+let url = 'https://netflix-api3.p.rapidapi.com/year/2021'
+let urlTab = url.split("/")
+// ====================== avoir inpur search et button serach par année
+let years = document.getElementById("years")
+let buttonYears = document.getElementById("validationYears")
 
-fetch('https://netflix-api3.p.rapidapi.com/year/2020', options)
-	.then(response => response.json())
-	.then(data => data[0].director)
-	.catch(err => console.error(err));
+// ====================== collback pour récuperer année
+buttonYears.addEventListener('click', e => {
+	
+	// ==================== Changer la valeur du url après utlisateur
+	urlTab[4] = years.value
+	// ==================== recréer un neauveau url
+	let urlFinal = urlTab.join("/")
+	console.log('----------------------->',urlFinal)
+	getAPIinfo(urlFinal, options)
+})
 
-console.log(data[0])
-
-
-//////
-
-function updateHTMLWithData(data) {
-	console.log(data[0].director)
-	document.getElementsByClassName('news')[0].textContent = data
-}
-
-function getAPIinfo() {
-	let url = 'https://....'
+// ======================= Avoir récuperé api avec ferch
+function getAPIinfo(url, options){
+	console.log("---------",url)
 	fetch(url, options)
-		.then((response) => {
+		.then(response => {
 			return response.json()
 		})
-		.then((jsonData) => {
-			console.log(jsonData)
-			updateHTMLWithData(jsonData)
+		.then(data => {
+			console.log(data)
 		})
-		.catch((err) => {
-			console.log('Une erreur est survenue')
-			console.error(err)
-		})
+		.catch(err => console.error(err));
 }
 
-getAPIinfo()
+
