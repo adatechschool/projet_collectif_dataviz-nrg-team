44cb1c50ccc0c1f11845915df950ba91e017c2a5
huor97 <huor97ka@gmail.com>
2022-11-30 14:33:53 +0100

    modifier par rouh pour affichage

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+
 // ==================== Avoir api
 const options = {
 	method: 'GET',
@@ -17,16 +18,18 @@ let inputDirectorUser = document.getElementById("directorsearchbar");
 let buttonYears = document.getElementById("validationYears");//bouton
 
 
+
 // == Collback event: à chaque click sur button envoie ce qui est dans bar du recherch
 buttonYears.addEventListener('click', e => {
 	// ajouter une condition qui prendre juste des chiffre et 4 chiffre, sinon affiche "votre demande ne pas trouvée"
 	// ==================== index 4 = aaaa qui prendre la valeur donnée par utilisateur
 	urlTab[4] = years.value
-	
+	console.log(urlFinal)
+	// filmresults 
 	// ==================== transférer tableau en url
 	let urlFinal = urlTab.join("/")
-	console.log('----------------------->',urlFinal)// chaeck nouveu url
 	getAPIinfo(urlFinal, options) // utiliser pour fetch
+
 })
 
 // ======================= Avoir récuperé api avec fetch
@@ -39,93 +42,117 @@ function getAPIinfo(url, options){
 
 		})
 		.then(data => {
-
+			console.log(data)
 			searchDataBase(data)
-			
-			separateShowTvAndMovie(data)
 			
 		})
 		.catch(err => console.error(err)); // retourner erreur si ne pas fonction data
 }
 
-function getTitleByCountryandTitle (country, title, listedIn, director){
-	console.log(inputCountryUser.value)
-let titleResult = [];
-    for (i = 0; i < country.length; i++){
-        if (
-			country[i].includes(inputCountryUser.value) == true && 
-			listedIn[i].includes(inputGenreUser.value) == true &&
-			director[i].includes(inputDirectorUser.value) ==true){
-            titleResult.push(title[i] +  " -- Director: " + director[i]);
-		}
-    }
-	document.getElementById("filmresults").innerHTML = titleResult.join("<br>");
-	console.log(titleResult);
-	//console.log(directorResult);
-}
-
-
+// ===================================== Récuperer la json et ajouter dans tableaux chaque valeur, years[0] = 1999
 function searchDataBase (jsonData){
-    let countryList = [];
+
+	console.log(jsonData)
+	let type = []
     let titleList = [];
-	let listedInlist = [];
+	let years = [];
 	let directorList = [];
+	let acteurs = [];
+	let listedInlist = []; // genre du film
+    let countryList = [];
+	let description = [];
+
     for (i = 0; i < jsonData.length; i++){
-        countryList.push(jsonData[i].country)
-        titleList.push(jsonData[i].title)
-		listedInlist.push(jsonData[i].listedIn)
-		directorList.push(jsonData [i].director)
-    }
-    getTitleByCountryandTitle (countryList, titleList, listedInlist, directorList);
-}
-// ============================= Avoir les film à partir de l'année
-function separateShowTvAndMovie(data){
-	
-	let emissionTV = [""]
-	let film = [""]
-	let j = 0
-	
-// ========================== Parcourire de donnée pour récuperer chaque valeur qui ont besoin
-	let i = 0;
-	while(i<=data.length-1){
 
-		if(data[i].type == "TV Show"){// avoir les émission télé
-			
-			emissionTV[j] = data[i];
+		if(jsonData[i].type == "TV Show"){// avoir les émission télé
 
-			j++
-		}else if (data[i].type == "Movie"){//avoir les film
+			type.push(jsonData[i].type)
+			titleList.push(jsonData[i].title) 
+			years.push(jsonData[i].releaseYear)
+			directorList.push(jsonData [i].director)
+			acteurs.push(jsonData[i].cast)
+			listedInlist.push(jsonData[i].listedIn)
+			countryList.push(jsonData[i].country)
+			description.push(jsonData[i].description)
+
+		} else if(jsonData[i].type == "Movie"){// avoir les film
 			
-			film.push(data[i])
-		
+			type.push(jsonData[i].type)
+			titleList.push(jsonData[i].title) 
+			years.push(jsonData[i].releaseYear)
+			directorList.push(jsonData [i].director)
+			acteurs.push(jsonData[i].cast)
+			listedInlist.push(jsonData[i].listedIn)
+			countryList.push(jsonData[i].country)
+			description.push(jsonData[i].description)
+
 		}
-		i++
-	}
-	showTvToYears(emissionTV)
-	filmToYears(film)
+    }
+
+    getTitleByCountryandTitle (type, titleList, years, directorList, acteurs, listedInlist, countryList, description);
 
 }
 
-let showTvAndFilm = document.getElementById("ShowTvAndFilm")// div qui contenir les émission télé et film 
+// ================================== Avoir les titre type annéee... d'un film dans condition les filtres du country, Genre et director
+function getTitleByCountryandTitle (type, title, years, director, acteurs, listedIn, country, description){
 
-// ======================== Avoir les emission télé
-function showTvToYears(emissionTv){
+	let result = []
+	let resultFinal = []
+	let j = 0
+	for (i = 0; i < country.length; i++){
+		if (
+				country[i].includes(inputCountryUser.value) == true && 
+				listedIn[i].includes(inputGenreUser.value) == true &&
+				director[i].includes(inputDirectorUser.value) ==true)
+				{
+					result.push(type[i] + title)// [movie]
+					// result.push(title[j])// [movie], [toto]
+					// console.log("i: ", i,result[i])
+					// resultFinal.push(result)
+
+				}
+				j++
+    }
+	// console.log(resultFinal)
+
+	afficheFilm(result)//Ajouter resulta du film dans fonction pour afficher sur page html 
 	
-	let i = 0;
-	while(i <= emissionTv.length-1){
-		let titleP = document.createElement("p")
-		let typeP = document.createElement("p")
-		titleP.innerText = emissionTv[i]["title"]
-		typeP.innerText = emissionTv[i]["type"]
-		showTvAndFilm.appendChild(titleP)
-		showTvAndFilm.appendChild(typeP)
-		
-		i++
-	}
+	let test = []
+	
+	test[0] = result[0].split(" ")
+	test[1] = result[1].split(" ")
+
+	// console.log(test)
+
+
 }
 
+// =========================== Cline html après chaque changement d'un api
+const removeChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
+// =========================== Avoir affiché les films sur page html
+function afficheFilm(result){
+	
+	let divResult;
+	let filmresults = document.getElementById("resultFilm");
+	removeChilds(filmresults)
+	let chaqueFilm = []
 
-// ======================== Avoir les film
-function filmToYears(film){
-	console.log("film: ", film)
+	for(let i = 0; i<result.length; i++){
+
+		chaqueFilm[i] = result[i].split(" ")
+
+	}
+
+	for(let i = 0; i< result.length; i++){
+
+		divResult = document.createElement("div")
+		divResult.className = "classFordevResult"
+		divResult.innerText = result[i]
+		filmresults.appendChild(divResult)
+	}
+
 }
